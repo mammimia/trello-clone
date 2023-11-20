0cd56f362a5a2799bdfa1f5e5ad6886ea19e13e9
Muhammed Ali AYDIN <muhammedaliaydin97@gmail.com>
2023-11-20 08:17:09 +0300

    🔀 refactor(mobile-sidebar.tsx): update import path for useMobileSidebar hook to match new file location
    🆕 feat(use-mobile-sidebar.tsx): create useMobileSidebar hook to manage state for mobile sidebar component

--- app/(platform)/(dashboard)/_components/mobile-sidebar.tsx
+++ app/(platform)/(dashboard)/_components/mobile-sidebar.tsx
@@ -1,6 +1,6 @@
 'use client';
 
-import { useMobileSidebar } from '@/app/hooks/use-mobile-sidebar';
+import { useMobileSidebar } from '@/hooks/use-mobile-sidebar';
 import { Button } from '@/components/ui/button';
 import { Sheet, SheetContent } from '@/components/ui/sheet';
 import { Menu } from 'lucide-react';
