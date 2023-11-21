a3a5488cfc923edb8df4e71fc66e4cdd7b7a2b92
Muhammed Ali AYDIN <muhammedaliaydin97@gmail.com>
2023-11-21 10:00:39 +0300

    ✨ feat(form-popover.tsx): add FormPopover component to create a popover form for creating a board
    The FormPopover component is added to create a popover form for creating a board. It includes the following features:
    - Imports necessary dependencies and components
    - Defines the FormPopoverProps interface for the component's props
    - Uses the useAction hook to handle the createBoard action
    - Defines an onSubmit function to handle form submission
    - Renders the popover content with the Create Board title and a close button
    - Renders a form with a FormInput component for the board title and a FormSubmit component for the create button

---
+++ components/form/form-popover.tsx
@@ -0,0 +1,81 @@
+'use client';
+
+import { X } from 'lucide-react';
+import { Button } from '../ui/button';
+import {
+  Popover,
+  PopoverClose,
+  PopoverContent,
+  PopoverTrigger
+} from '../ui/popover';
+import { FormInput } from './form-input';
+import { FormSubmit } from './form-submit';
+import { useAction } from '@/hooks/use-action';
+import { createBoard } from '@/actions/create-board';
+
+interface FormPopoverProps {
+  children: React.ReactNode;
+  side?: 'top' | 'bottom' | 'left' | 'right';
+  align?: 'start' | 'center' | 'end';
+  sideOffset?: number;
+}
+
+export const FormPopover = ({
+  children,
+  side = 'bottom',
+  align,
+  sideOffset = 0
+}: FormPopoverProps) => {
+  const { execute, fieldErrors } = useAction(createBoard, {
+    onSuccess: (data) => {
+      console.log({ data });
+    },
+    onError: (error) => {
+      console.log({ error });
+    }
+  });
+
+  const onSubmit = (formData: FormData) => {
+    const title = formData.get('title') as string;
+    execute({ title });
+  };
+
+  return (
+    <Popover>
+      <PopoverTrigger>{children}</PopoverTrigger>
+      <PopoverContent
+        side={side}
+        align={align}
+        sideOffset={sideOffset}
+        className="w-80 pt-3 "
+      >
+        <div
+          className="pb-4 text-center text-sm
+         font-medium text-neutral-600"
+        >
+          Create Board
+        </div>
+        <PopoverClose asChild>
+          <Button
+            className="absolute right-2 top-2 h-auto
+           w-auto p-2 text-neutral-600"
+            variant="ghost"
+          >
+            <X className="h-4 w-4" />
+          </Button>
+        </PopoverClose>
+        <form action={onSubmit} className="space-y-4">
+          <div className="space-y-4">
+            <FormInput
+              id="title"
+              label="Board Title"
+              type="text"
+              errors={fieldErrors}
+            />
+          </div>
+          <FormSubmit className="w-full">Create</FormSubmit>
+        </form>
+      </PopoverContent>
+    </Popover>
+  );
+};
