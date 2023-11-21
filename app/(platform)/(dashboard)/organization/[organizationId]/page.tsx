1848ed65bf6505d20df39c2e912c24d7d27c98a7
Muhammed Ali AYDIN <muhammedaliaydin97@gmail.com>
2023-11-21 08:06:08 +0300

    📝 feat(board-list.tsx): add BoardList component to display user's boards in the organization page 📝 feat(page.tsx): import and render BoardList component in the organization page

--- app/(platform)/(dashboard)/organization/[organizationId]/page.tsx
+++ app/(platform)/(dashboard)/organization/[organizationId]/page.tsx
@@ -1,5 +1,7 @@
 import { NextPage } from 'next';
 import { Info } from './_components/info';
+import { Separator } from '@/components/ui/separator';
+import { BoardList } from './_components/board-list';
 
 interface Props {}
 
@@ -7,6 +9,10 @@ const OrganizationIdPage: NextPage<Props> = async ({}) => {
   return (
     <div className="mb-20 w-full">
       <Info />
+      <Separator className="my-4" />
+      <div className="px-2 md:px-4">
+        <BoardList />
+      </div>
     </div>
   );
 };
