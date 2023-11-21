9f7b6231a4a83bbd5b2ca9c0fb6cfd411ea96bca
Muhammed Ali AYDIN <muhammedaliaydin97@gmail.com>
2023-11-21 07:55:03 +0300

    🐛 fix(info.tsx): import Skeleton component from correct path to fix module resolution issue ✨ feat(info.tsx): add skeleton loading state to improve user experience while data is being loaded

--- app/(platform)/(dashboard)/organization/[organizationId]/_components/info.tsx
+++ app/(platform)/(dashboard)/organization/[organizationId]/_components/info.tsx
@@ -1,5 +1,6 @@
 'use client';
 
+import { Skeleton } from '@/components/ui/skeleton';
 import { useOrganization } from '@clerk/nextjs';
 import { CreditCard } from 'lucide-react';
 import Image from 'next/image';
@@ -7,7 +8,7 @@ import Image from 'next/image';
 export const Info = () => {
   const { organization, isLoaded } = useOrganization();
 
-  if (!isLoaded) return <div>Loading...</div>;
+  if (!isLoaded) return <Info.Skeleton />;
 
   return (
     <div className="flex items-center gap-x-4">
@@ -24,6 +25,23 @@ export const Info = () => {
         <div className="item-center flex text-xs text-muted-foreground">
           <CreditCard className="mr-1 h-3 w-3" />
           Free
+        </div>
+      </div>
+    </div>
+  );
+};
+
+Info.Skeleton = function InfoSkeleton() {
+  return (
+    <div className="flex items-center gap-x-4">
+      <div className="relative h-[60px] w-[60px]">
+        <Skeleton className="absolute h-full w-full" />
+      </div>
+      <div className="space-y-1">
+        <Skeleton className="h-10 w-[200px]" />
+        <div className="flex items-center">
+          <Skeleton className="mr-2 h-4 w-4" />
+          <Skeleton className="h-4 w-[100px]" />
         </div>
       </div>
     </div>
