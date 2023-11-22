879fcab0cbd8412d03a8f930682341d54d3fc373
Muhammed Ali AYDIN <muhammedaliaydin97@gmail.com>
2023-11-22 16:42:32 +0300

    🔧 fix(layout.tsx): add generateMetadata function to generate title metadata for the organization layout 📦 chore(package.json): add lodash and @types/lodash as dependencies to use startCase function in layout.tsx

--- app/(platform)/(dashboard)/organization/[organizationId]/layout.tsx
+++ app/(platform)/(dashboard)/organization/[organizationId]/layout.tsx
@@ -1,5 +1,15 @@
 import { NextPage } from 'next';
 import OrganizationControl from './_components/organization-control';
+import { startCase } from 'lodash';
+import { auth } from '@clerk/nextjs';
+
+export async function generateMetadata() {
+  const { orgSlug } = auth();
+
+  return {
+    title: startCase(orgSlug || 'organization')
+  };
+}
 
 interface Props {
   children: React.ReactNode;
