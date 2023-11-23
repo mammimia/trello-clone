66239e6b59373e8a9fee258e79c590173516aa1d
Muhammed Ali AYDIN <muhammedaliaydin97@gmail.com>
2023-11-23 19:39:49 +0300

    📦 chore(update-board): add update-board action
    📄 actions/update-board/index.ts:
    - Create a new file `actions/update-board/index.ts` to handle the update-board action.
    - Import necessary dependencies and types.
    - Define the `handler` function to handle the update-board action.
    - Authenticate the user using `auth()` function from `@clerk/nextjs`.
    - Check if the user is authenticated, return an error if not.
    - Extract the `id` and `title` from the input data.
    - Use `db.board.update` to update the board in the database.
    - Catch any errors and return an error message if the update fails.
    - Call `revalidatePath` to invalidate the cache for the board page.
    - Return the updated board data.
    
    📄 actions/update-board/schema.ts:
    - Create a new file `actions/update-board/schema.ts` to define the schema for the update-board action input.
    - Import `z` from `zod`.
    - Define the `UpdateBoard` schema with `id` and `title` fields.
    - Add validation rules for the `title` field.
    
    📄 actions/update-board/types.ts:
    - Create a new file `actions/update-board/types.ts` to define the input and output types for the update-board action.
    - Import necessary dependencies and types.
    - Define `InputType` as the inferred type of `UpdateBoard` schema.
    - Define `OutputType` as the `ActionState` type with `InputType` and `Board` from `@prisma/client`.

---
+++ actions/update-board/index.ts
@@ -0,0 +1,38 @@
+'use server';
+
+import { auth } from '@clerk/nextjs';
+import { InputType, OutputType } from './types';
+import { db } from '@/lib/db';
+import { revalidatePath } from 'next/cache';
+import { createSafeAction } from '@/lib/create-safe-action';
+import { UpdateBoard } from './schema';
+
+const handler = async (data: InputType): Promise<OutputType> => {
+  const { userId, orgId } = auth();
+
+  if (!userId || !orgId) {
+    return { error: 'Not authenticated' };
+  }
+
+  const { id, title } = data;
+  let board;
+
+  try {
+    board = await db.board.update({
+      where: {
+        id,
+        orgId
+      },
+      data: {
+        title
+      }
+    });
+  } catch (error) {
+    return { error: 'Failed to update board!' };
+  }
+
+  revalidatePath(`/boards/${id}`);
+  return { data: board };
+};
+
+export const updateBoard = createSafeAction(UpdateBoard, handler);
