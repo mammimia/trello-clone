e80cce1363bccdf05f33d483b2ee4c64a7ea0bf7
Muhammed Ali AYDIN <muhammedaliaydin97@gmail.com>
2023-11-24 11:35:52 +0300

    🔧 fix(list-header.tsx): fix import statement for FormInput component to use absolute path ✨ feat(list-header.tsx): add functionality to enable editing of list title on click ✨ feat(list-header.tsx): add functionality to disable editing of list title on blur ✨ feat(list-header.tsx): add functionality to submit form on pressing Escape key ✨ feat(list-header.tsx): add event listener for Escape key press using useEventListener hook ✨ feat(list-header.tsx): add form and input elements for editing list title ✨ feat(list-header.tsx): add logic to show input field when editing is enabled and show div when editing is disabled ✨ feat(list-header.tsx): add logic to select input field and focus on it when editing is enabled

--- app/(platform)/(dashboard)/board/[boardId]/_components/list-header.tsx
+++ app/(platform)/(dashboard)/board/[boardId]/_components/list-header.tsx
@@ -1,22 +1,65 @@
 'use client';
+import { FormInput } from '@/components/form/form-input';
 import { List } from '@prisma/client';
+import { ElementRef, useRef, useState } from 'react';
+import { useEventListener } from 'usehooks-ts';
 
 interface ListHeaderProps {
   list: List;
 }
 
 export const ListHeader = ({ list }: ListHeaderProps) => {
+  const [title, setTitle] = useState(list.title);
+  const [isEditing, setIsEditing] = useState(false);
+
+  const formRef = useRef<ElementRef<'form'>>(null);
+  const inputRef = useRef<ElementRef<'input'>>(null);
+
+  const enableEditing = () => {
+    setIsEditing(true);
+    setTimeout(() => inputRef.current?.select());
+  };
+
+  const disableEditing = () => {
+    setIsEditing(false);
+  };
+
+  const onKeydown = (event: KeyboardEvent) => {
+    if (event.key === 'Escape') {
+      formRef.current?.requestSubmit();
+    }
+  };
+
+  useEventListener('keydown', onKeydown);
+
   return (
     <div
       className="flex items-start justify-between
      gap-x-2 px-2 pt-2 text-sm font-semibold"
     >
-      <div
-        className="h-7 w-full border-transparent px-2.5 
+      {isEditing ? (
+        <form className="flex-1 px-[2px]">
+          <input hidden id="id" name="id" value={list.id} />
+          <input hidden id="boardId" name="boardId" value={list.boardId} />
+          <FormInput
+            className="h-7 truncate border-transparent bg-transparent px-[7px] py-1
+             text-sm font-medium transition hover:border-input focus:border-input focus:bg-white"
+            ref={inputRef}
+            onBlur={disableEditing}
+            id={title}
+            placeholder="Enter list title..."
+            defaultValue={title}
+          />
+        </form>
+      ) : (
+        <div
+          className="h-7 w-full border-transparent px-2.5 
       py-1 text-sm font-medium"
-      >
-        {list.title}
-      </div>
+          onClick={enableEditing}
+        >
+          {title}
+        </div>
+      )}
     </div>
   );
 };
