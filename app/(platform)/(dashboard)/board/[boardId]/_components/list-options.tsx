a98de424793b8075dd5be46b4a4f44026dc384de
Muhammed Ali AYDIN <muhammedaliaydin97@gmail.com>
2023-11-25 20:16:25 +0300

    ✨ feat(list-options.tsx): add copy functionality to copy a list and show error toast on copy failure

--- app/(platform)/(dashboard)/board/[boardId]/_components/list-options.tsx
+++ app/(platform)/(dashboard)/board/[boardId]/_components/list-options.tsx
@@ -1,5 +1,6 @@
 'use client';
 
+import { copyList } from '@/actions/copy-list';
 import { deleteList } from '@/actions/delete-list';
 import { FormSubmit } from '@/components/form/form-submit';
 import { Button } from '@/components/ui/button';
@@ -34,11 +35,28 @@ const ListOptions = ({ list, onAddCard }: ListOptionsProps) => {
     }
   });
 
+  const { execute: executeCopy } = useAction(copyList, {
+    onSuccess: (data) => {
+      toast.success(`List ${data.title} copied`);
+      closeRef.current?.click();
+    },
+    onError: (error) => {
+      toast.error(error);
+    }
+  });
+
   const onDelete = (formData: FormData) => {
     const id = formData.get('id') as string;
     const boardId = formData.get('boardId') as string;
 
     executeDelete({ id, boardId });
+  };
+
+  const onCopy = (formData: FormData) => {
+    const id = formData.get('id') as string;
+    const boardId = formData.get('boardId') as string;
+
+    executeCopy({ id, boardId });
   };
 
   return (
@@ -68,7 +86,7 @@ const ListOptions = ({ list, onAddCard }: ListOptionsProps) => {
         >
           Add card
         </Button>
-        <form>
+        <form action={onCopy}>
           <input hidden id="id" name="id" value={list.id} />
           <input hidden id="boardId" name="boardId" value={list.boardId} />
           <FormSubmit
