c4ffc348825b8eead06499152eee83c503af51dd
Muhammed Ali AYDIN <muhammedaliaydin97@gmail.com>
2023-11-26 12:49:03 +0300

    ✨ feat(card-item.tsx): add drag and drop functionality to card items

--- app/(platform)/(dashboard)/board/[boardId]/_components/card-item.tsx
+++ app/(platform)/(dashboard)/board/[boardId]/_components/card-item.tsx
@@ -1,3 +1,4 @@
+import { Draggable } from '@hello-pangea/dnd';
 import { Card } from '@prisma/client';
 import React from 'react';
 
@@ -8,12 +9,19 @@ interface CardItemProps {
 
 export const CardItem = ({ card, index }: CardItemProps) => {
   return (
-    <div
-      role="button"
-      className="truncate rounded-md border-2 border-transparent
-     bg-white px-3 py-2 text-sm shadow-sm hover:border-black/50"
-    >
-      {card.title}
-    </div>
+    <Draggable draggableId={card.id} index={index}>
+      {(provided) => (
+        <div
+          {...provided.draggableProps}
+          {...provided.dragHandleProps}
+          ref={provided.innerRef}
+          role="button"
+          className="truncate rounded-md border-2 border-transparent
+           bg-white px-3 py-2 text-sm shadow-sm hover:border-black/50"
+        >
+          {card.title}
+        </div>
+      )}
+    </Draggable>
   );
 };
