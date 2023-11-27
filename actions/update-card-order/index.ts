7b583df773125a4324bc31365118ee423f7bd2f2
Muhammed Ali AYDIN <muhammedaliaydin97@gmail.com>
2023-11-27 09:04:13 +0300

    📦 chore(update-card-order): add update card order action

---
+++ actions/update-card-order/index.ts
@@ -0,0 +1,51 @@
+'use server';
+
+import { createSafeAction } from '@/lib/create-safe-action';
+import { db } from '@/lib/db';
+import { auth } from '@clerk/nextjs';
+import { revalidatePath } from 'next/cache';
+import { UpdateCardOrder } from './schema';
+import { InputType, OutputType } from './types';
+
+const handler = async (data: InputType): Promise<OutputType> => {
+  const { userId, orgId } = auth();
+
+  if (!userId || !orgId) {
+    return {
+      error: 'Not authenticated'
+    };
+  }
+
+  const { items, boardId } = data;
+  let cards;
+
+  try {
+    const transaction = items.map((card) =>
+      db.card.update({
+        where: {
+          id: card.id,
+          list: {
+            board: {
+              orgId
+            }
+          }
+        },
+        data: {
+          order: card.order,
+          listId: card.listId
+        }
+      })
+    );
+
+    cards = await db.$transaction(transaction);
+  } catch (error) {
+    return {
+      error: 'Failed to reorder card!'
+    };
+  }
+
+  revalidatePath(`/boards/${boardId}`);
+  return { data: cards };
+};
+
+export const updateCardOrder = createSafeAction(UpdateCardOrder, handler);
