ff183529e2293104f03f5d0d59f77cead9f3a723
Muhammed Ali AYDIN <muhammedaliaydin97@gmail.com>
2023-11-27 10:39:16 +0300

    ✨ feat(query-provider.tsx): add query provider to enable @tanstack/react-query and wrap platform layout with it

--- app/(platform)/layout.tsx
+++ app/(platform)/layout.tsx
@@ -1,4 +1,5 @@
 import { ModalProvider } from '@/components/providers/modal-provider';
+import { QueryProvider } from '@/components/providers/query-provider';
 import { ClerkProvider } from '@clerk/nextjs';
 import { NextPage } from 'next';
 import { Toaster } from 'sonner';
@@ -10,9 +11,11 @@ interface Props {
 const PlatformLayout: NextPage<Props> = ({ children }) => {
   return (
     <ClerkProvider>
-      <Toaster />
-      <ModalProvider />
-      {children}
+      <QueryProvider>
+        <Toaster />
+        <ModalProvider />
+        {children}
+      </QueryProvider>
     </ClerkProvider>
   );
 };
