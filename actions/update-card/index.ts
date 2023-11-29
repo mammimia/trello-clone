99e8c3cd3a33026a60768d62b31475a8ab27a0a9
Muhammed Ali AYDIN <muhammedaliaydin97@gmail.com>
2023-11-29 08:13:39 +0300

    🎉 feat(index.tsx): add audit log creation when updating a card

--- actions/update-card/index.ts
+++ actions/update-card/index.ts
@@ -1,8 +1,10 @@
 'use server';
 
+import { createAuditLog } from '@/lib/create-audit-log';
 import { createSafeAction } from '@/lib/create-safe-action';
 import { db } from '@/lib/db';
 import { auth } from '@clerk/nextjs';
+import { ACTION, ENTITIY_TYPE } from '@prisma/client';
 import { revalidatePath } from 'next/cache';
 import { UpdateCard } from './schema';
 import { InputType, OutputType } from './types';
@@ -30,6 +32,13 @@ const handler = async (data: InputType): Promise<OutputType> => {
       data: {
         ...values
       }
+    });
+
+    await createAuditLog({
+      entityTitle: card.title,
+      entityId: card.id,
+      entityType: ENTITIY_TYPE.CARD,
+      action: ACTION.UPDATE
     });
   } catch (error) {
     return { error: 'Failed to update card!' };
