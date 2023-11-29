5e5a351f726eb772383e77bedf2993914c8518f6
Muhammed Ali AYDIN <muhammedaliaydin97@gmail.com>
2023-11-29 08:10:20 +0300

    🎉 feat(index.tsx): add audit log creation when deleting a board

--- actions/delete-board/index.ts
+++ actions/delete-board/index.ts
@@ -1,8 +1,10 @@
 'use server';
 
+import { createAuditLog } from '@/lib/create-audit-log';
 import { createSafeAction } from '@/lib/create-safe-action';
 import { db } from '@/lib/db';
 import { auth } from '@clerk/nextjs';
+import { ACTION, ENTITIY_TYPE } from '@prisma/client';
 import { revalidatePath } from 'next/cache';
 import { redirect } from 'next/navigation';
 import { DeleteBoard } from './schema';
@@ -24,6 +26,13 @@ const handler = async (data: InputType): Promise<OutputType> => {
         id,
         orgId
       }
+    });
+
+    await createAuditLog({
+      entityTitle: board.title,
+      entityId: board.id,
+      entityType: ENTITIY_TYPE.BOARD,
+      action: ACTION.DELETE
     });
   } catch (error) {
     return { error: 'Failed to delete board!' };
