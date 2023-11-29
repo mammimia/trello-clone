78d4eff337bb9deea470a9cb1178e3fade379449
Muhammed Ali AYDIN <muhammedaliaydin97@gmail.com>
2023-11-29 08:40:44 +0300

    ✨ feat(avatar.tsx): add activity list component to organization dashboard

---
+++ app/(platform)/(dashboard)/organization/[organizationId]/activity/_components/activity-list.tsx
@@ -0,0 +1,48 @@
+import { ActivityItem } from '@/components/activity-item';
+import { Skeleton } from '@/components/ui/skeleton';
+import { db } from '@/lib/db';
+import { auth } from '@clerk/nextjs';
+import { redirect } from 'next/navigation';
+
+export const ActivityList = async () => {
+  const { orgId } = auth();
+
+  if (!orgId) redirect('/select-organization');
+
+  const auditLogs = await db.auditLog.findMany({
+    where: {
+      orgId
+    },
+    orderBy: {
+      createdAt: 'desc'
+    }
+  });
+  return (
+    <ol className="mt-4 space-y-4">
+      <p
+        className="hidden text-center text-xs 
+  text-muted-foreground last:block"
+      >
+        No activity found inside this organization.
+      </p>
+      {auditLogs.map((activity) => (
+        <ActivityItem key={activity.id} activity={activity} />
+      ))}
+    </ol>
+  );
+};
+
+ActivityList.Skeleton = function ActivityListSkeleton() {
+  return (
+    <ol className="mt-4 space-y-4">
+      <Skeleton className="h-14 w-[80%]" />
+      <Skeleton className="h-14 w-[60%]" />
+      <Skeleton className="h-14 w-[50%]" />
+      <Skeleton className="h-14 w-[70%]" />
+      <Skeleton className="h-14 w-[80%]" />
+      <Skeleton className="h-14 w-[80%]" />
+      <Skeleton className="h-14 w-[60%]" />
+      <Skeleton className="h-14 w-[70%]" />
+    </ol>
+  );
+};
